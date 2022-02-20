8e8c96758c770906560c46fa6ff78dba6a867268
Tomeski05 <tomeski@protonmail.com>
2022-02-20 23:42:28 +0100

    Classes and Instances

--- Classes and Instances/Classes and Instances/Classes and Instances.cs
+++ Classes and Instances/Classes and Instances/Classes and Instances.cs
@@ -152,6 +152,28 @@ namespace Classes_and_Instances
 
             #endregion
 
+            #region Task7
+
+            // Test constructor and toString()
+            //Date d1 = new Date(1, 2, 2014);
+            //Console.WriteLine(d1.toString());
+
+            //// Test Setters and Getters
+            //d1.setMonth(12);
+            //d1.setDay(9);
+            //d1.setYear(2099);
+            //Console.WriteLine(d1.toString());
+            //Console.WriteLine("Month: " + d1.getMonth());
+            //Console.WriteLine("Day: " + d1.getDay());
+            //Console.WriteLine("Year: " + d1.getYear());
+
+            //// Test setDate()
+            //d1.setDate(3, 4, 2016);
+            //Console.WriteLine(d1.toString());
+
+
+            #endregion
+
             Console.ReadLine();
         }
 
@@ -513,6 +535,67 @@ namespace Classes_and_Instances
         //    {
         //        return $"Account with id {id}, name {name} and balance {balance}";
         //    }
+        //}
+
+        #endregion
+
+        #region Task7
+
+        //public class Date
+        //{
+        //    private int day;
+        //    private int month;
+        //    private int year;
+
+        //    public Date(int day, int month, int year)
+        //    {
+        //        this.day = day;
+        //        this.month = month;
+        //        this.year = year;
+        //    }
+
+        //    public int getDay()
+        //    {
+        //        return day;
+        //    }
+
+        //    public int getMonth()
+        //    {
+        //        return month;
+        //    }
+
+        //    public int getYear()
+        //    {
+        //        return year;
+        //    }
+
+        //    public void setDay(int day)
+        //    {
+        //        this.day = day;
+        //    }
+
+        //    public void setMonth(int month)
+        //    {
+        //        this.month = month;
+        //    }
+
+        //    public void setYear(int year)
+        //    {
+        //        this.year = year;
+        //    }
+
+        //    public void setDate(int day, int month, int year)
+        //    {
+        //        this.day = day;
+        //        this.month = month;
+        //        this.year = year;
+        //    }
+
+        //    public string toString()
+        //    {
+        //        return $"Day {day} month {month} year {year}";
+        //    }
+
         //}
 
         #endregion
