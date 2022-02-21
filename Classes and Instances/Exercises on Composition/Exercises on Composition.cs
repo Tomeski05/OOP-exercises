f21298f230a6d1d8cd98ba7179c49dfcef242971
Tomeski05 <tomeski@protonmail.com>
2022-02-21 23:01:56 +0100

    Exercises on Composition

--- Classes and Instances/Exercises on Composition/Exercises on Composition.cs
+++ Classes and Instances/Exercises on Composition/Exercises on Composition.cs
@@ -84,6 +84,19 @@ namespace Exercises_on_Composition
 
             #region Task4
 
+            //Account account = new Account(1, new Customer(2, "Ha Gia Kinh", 'm'), 10000.0);
+            //Console.WriteLine(account.toString());
+            //account.withdraw(100);
+            //Console.WriteLine(account);
+            //Console.WriteLine(account.getCustomerName());
+            //account.deposit(500);
+            //Console.WriteLine(account);
+
+            #endregion
+
+            #region Task5
+
+
 
             #endregion
 
@@ -437,42 +450,120 @@ namespace Exercises_on_Composition
 
         #region Task4
 
-        public class Customer
-        {
-            private int ID;
-            private string name;
-            private char gender;
+        //public class Customer
+        //{
+        //    private int ID;
+        //    private string name;
+        //    private char gender;
 
-            public Customer(int id, string name, char gender)
-            {
-                this.ID = id;
-                this.name = name;
-                this.gender = gender;
-            }
+        //    public Customer(int id, string name, char gender)
+        //    {
+        //        this.ID = id;
+        //        this.name = name;
+        //        this.gender = gender;
+        //    }
 
-            public int getID()
-            {
-                return ID;
-            }
+        //    public int getID()
+        //    {
+        //        return ID;
+        //    }
 
-            public string getName()
-            {
-                return name;
-            }
+        //    public string getName()
+        //    {
+        //        return name;
+        //    }
 
-            public char getGender()
-            {
-                return gender;
-            }
+        //    public char getGender()
+        //    {
+        //        return gender;
+        //    }
 
-            public string toString()
-            {
-                return $"Name {name} with id {ID}";
-            }
+        //    public string toString()
+        //    {
+        //        return $"Name {name} with id {ID}";
+        //    }
 
 
+        //    public class Account
+        //    {
+        //        private int id;
+        //        private Customer customer;
+        //        private double balance;
 
-        }
+        //        public Account(int id, Customer customer, double balance)
+        //        {
+        //            this.id = id;
+        //            this.customer = customer;
+        //            this.balance = balance;
+        //        }
+
+        //        public Account(int id, Customer customer)
+        //        {
+        //            this.id = id;
+        //            this.customer = customer;
+        //        }
+
+        //        public int getId()
+        //        {
+        //            return id;
+        //        }
+
+        //        public void setId(int id)
+        //        {
+        //            this.id = id;
+        //        }
+
+        //        public Customer getCustomer()
+        //        {
+        //            return customer;
+        //        }
+
+        //        public void setCustomer(Customer customer)
+        //        {
+        //            this.customer = customer;
+        //        }
+
+        //        public double getBalance()
+        //        {
+        //            return balance;
+        //        }
+
+        //        public void setBalance(double balance)
+        //        {
+        //            this.balance = balance;
+        //        }
+
+        //        public String getCustomerName()
+        //        {
+        //            return this.customer.getName();
+        //        }
+
+        //        public Account deposit(double amount)
+        //        {
+        //            this.balance += amount;
+        //            return this;
+        //        }
+
+        //        public Account withdraw(double amount, double balance)
+        //        {
+        //            if (amount > this.balance)
+        //            {
+        //                Console.WriteLine("amount withdraw exceeds the current balance!");
+        //            }
+        //            else
+        //            {
+        //                this.balance -= amount;
+        //            }
+        //            return this;
+        //        }
+        //    }
+        //}
+
+        #endregion
+
+        #region Task5
+
+
 
         #endregion
 
