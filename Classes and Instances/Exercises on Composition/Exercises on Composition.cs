c9207a817ccee2f36cd8760209b58f0cff2440c5
Tomeski05 <tomeski@protonmail.com>
2022-02-21 15:32:23 +0100

    Exercises on Composition

---
+++ Classes and Instances/Exercises on Composition/Exercises on Composition.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercises_on_Composition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
